eccaecdc0584bf83667209d7ccfe2c655f912925
Анна Орловская <kostet14091990@gmail.com>
2020-03-31 12:45:49 +0300

    Практика

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,13 +6,39 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            var p1 = new Point(1,3,'*');
-            p1.Draw();
+            int x = 1;
+            Func1(x);
+            Func2(++x);
+            Func3(++x);
 
-            var p2 = new Point(4,5,'#');
-            p2.Draw();
+            Point p1 = new Point(1, 3, '*');
+            Move(p1, 10, 10);
 
-            Console.Read();
+            Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine($"p1 = p2. p1.x = {p1.x}  p1.y =  {p1.y}, p2.x = {p2.x}, p2.y = {p2.y}");
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+
+            Console.ReadLine();
         }
+
+        public static void Func1(int value) => Console.WriteLine("Func1.x = " + value);
+
+        public static void Func2(int value) => Console.WriteLine("Func2.x = " + value);
+
+        public static void Func3(int value) => Console.WriteLine("Func3.x = " + value);
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x += dx;
+            p.y += dy;
+            Console.WriteLine($"Call Move. p1.x = {p.x},  p1.y = {p.y}");
+        }
+
+        public static void Update(Point p) => Console.WriteLine($"Call Update. p1.x = {p.x}, p1.y = {p.y}");
     }
 }
